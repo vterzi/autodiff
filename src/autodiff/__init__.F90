c923747111b03c1095f6498dcff3289a5387047c
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2025-06-21 15:17:44 +0200

    Improve preprocessor code

--- src/autodiff/__init__.F90
+++ src/autodiff/__init__.F90
@@ -2,8 +2,8 @@
 #include "init.inc"
 
 #define _NAME _CAT3(_DERIV,_TYPE_LABEL,_KIND_LABEL)
-#define _UNARY(X) _CAT3(X,_,_NAME)
-#define _BINARY(X) _CAT5(_NAME,_,X,_,_NAME)
+#define _UNARY_TYPE_OP(X) _CAT3(X,_,_NAME)
+#define _BINARY_TYPE_OP(X) _CAT5(_NAME,_,X,_,_NAME)
 
 module autodiff
     use, intrinsic :: ieee_arithmetic, only: ieee_value, IEEE_QUIET_NAN
@@ -17,67 +17,46 @@ module autodiff
 
 #define _PROC(X) _CAT5(_OP,_,_DERIV,_TYPE_LABEL,X)
 #define _FILE "decl.inc"
-
 #define _OP real
 #include "iface_c.inc"
-
 #define _OP aimag
 #include "iface_c.inc"
-
 #define _OP conjg
 #include "iface_c.inc"
-
 #define _OP abs
 #include "iface_irc.inc"
-
 #define _OP sqrt
 #include "iface_rc.inc"
-
 #define _OP exp
 #include "iface_rc.inc"
-
 #define _OP log
 #include "iface_rc.inc"
-
 #define _OP log10
 #include "iface_r.inc"
-
 #define _OP sin
 #include "iface_rc.inc"
-
 #define _OP cos
 #include "iface_rc.inc"
-
 #define _OP tan
 #include "iface_rc.inc"
-
 #define _OP asin
 #include "iface_rc.inc"
-
 #define _OP acos
 #include "iface_rc.inc"
-
 #define _OP atan
 #include "iface_rc.inc"
-
 #define _OP sinh
 #include "iface_rc.inc"
-
 #define _OP cosh
 #include "iface_rc.inc"
-
 #define _OP tanh
 #include "iface_rc.inc"
-
 #define _OP asinh
 #include "iface_rc.inc"
-
 #define _OP acosh
 #include "iface_rc.inc"
-
 #define _OP atanh
 #include "iface_rc.inc"
-
 #undef _PROC
 #undef _FILE
 
