e3af805b0f50c328cb1e738a590cab9c36098e0b
Vladimir Terzi <vladimir.terzi@rwth-aachen.de>
2025-11-15 23:30:33 +0100

    Improve test_constructors

--- tests/test_constructors.F90
+++ tests/test_constructors.F90
@@ -4,16 +4,20 @@ program test_constructors
 
     implicit none
 
+    integer :: i
     real :: v, dg
-    real, allocatable :: g(:), gg(:, :)
+    real, allocatable :: g(:), gg(:, :), gg_(:, :)
     type(GradRSP) :: go
     type(GradgradRSP) :: ggo
     type(DivgradRSP) :: dgo
 
     v = 1.0
     g = [2.0, 3.0]
-    gg = reshape([4.0, 5.0, 5.0, 6.0], [2, 2])
-    dg = 10.0
+    gg = transpose(reshape([ &
+        4.0, 5.0, &
+        5.0, 6.0 &
+    ], [2, 2]))
+    dg = sum([(gg(i, i), i = 1, size(gg, 1))])
 
     go = GradRSP(v, g)
     call assert_equal(go%val(), v, file=__FILE__, line=__LINE__)
@@ -29,4 +33,59 @@ program test_constructors
     call assert_equal(dgo%val(), v, file=__FILE__, line=__LINE__)
     call assert_equal(dgo%grad(), g, file=__FILE__, line=__LINE__)
     call assert_equal(dgo%divgrad(), dg, file=__FILE__, line=__LINE__)
+
+    gg_ = transpose(reshape([ &
+        4.0, 5.0, &
+        0.0, 6.0 &
+    ], [2, 2]))
+    ggo = GradgradRSP(v, g, gg_, .true.)
+    call assert_equal(ggo%gradgrad(), gg, file=__FILE__, line=__LINE__)
+    gg_ = transpose(reshape([ &
+        4.0, 0.0, &
+        5.0, 6.0 &
+    ], [2, 2]))
+    ggo = GradgradRSP(v, g, gg_, .false.)
+    call assert_equal(ggo%gradgrad(), gg, file=__FILE__, line=__LINE__)
+    gg_ = transpose(reshape([ &
+        4.0, 5.0, &
+        5.0, 6.0, &
+        7.0, 8.0 &
+    ], [2, 3]))
+    ggo = GradgradRSP(v, g, gg_, .true.)
+    call assert_equal(ggo%gradgrad(), gg, file=__FILE__, line=__LINE__)
+    gg_ = transpose(reshape([ &
+        4.0, 5.0, 7.0, &
+        5.0, 6.0, 8.0 &
+    ], [3, 2]))
+    ggo = GradgradRSP(v, g, gg_, .true.)
+    call assert_equal(ggo%gradgrad(), gg, file=__FILE__, line=__LINE__)
+
+    gg = transpose(reshape([ &
+        4.0, 5.0, &
+        5.0, 0.0 &
+    ], [2, 2]))
+
+    gg_ = transpose(reshape([ &
+        4.0, 5.0 &
+    ], [2, 1]))
+    ggo = GradgradRSP(v, g, gg_, .true.)
+    call assert_equal(ggo%gradgrad(), gg, file=__FILE__, line=__LINE__)
+    gg_ = transpose(reshape([ &
+        4.0, 5.0, 7.0 &
+    ], [3, 1]))
+    ggo = GradgradRSP(v, g, gg_, .true.)
+    call assert_equal(ggo%gradgrad(), gg, file=__FILE__, line=__LINE__)
+    gg_ = transpose(reshape([ &
+        4.0, &
+        5.0 &
+    ], [1, 2]))
+    ggo = GradgradRSP(v, g, gg_, .false.)
+    call assert_equal(ggo%gradgrad(), gg, file=__FILE__, line=__LINE__)
+    gg_ = transpose(reshape([ &
+        4.0, &
+        5.0, &
+        7.0 &
+    ], [1, 3]))
+    ggo = GradgradRSP(v, g, gg_, .false.)
+    call assert_equal(ggo%gradgrad(), gg, file=__FILE__, line=__LINE__)
 end program test_constructors
